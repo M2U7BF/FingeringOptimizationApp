1d0434d4287b05db2a2e5cd17d036e6b57da808a
M2U7BF <yhvh4321@gmail.com>
2022-08-16 10:00:58 +0900

    [修正] 各メソッドの引数、ソートの部分を修正

--- src/main/java/service/Process.java
+++ src/main/java/service/Process.java
@@ -5,20 +5,18 @@ import form.ConditionForm;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 @Service
 public class Process {
-    //TODO : 分散の戻り値が1つなのでそこを精査する
 
 //    private int stringNumber;
 //    private int fretNumber;
     private int[] pressingPosition;
-    private List<int[]> candidate = new ArrayList<>();
-    private List<List<int[]>> candidates = new ArrayList<>();
     private List<int[]> result = new ArrayList<>();
     private List<List<int[]>> results = new ArrayList<>();
-    private List<Double> varianceList = new ArrayList<>();
 
     private Guitar guitar;
 
@@ -30,16 +28,37 @@ public class Process {
         List<List<int[]>> candidates = new ArrayList<>();
 
         for (String melody : melodies) {
+            System.out.println("melodies : "+melodies.size());
             guitar = new Guitar();
 
-            candidate = comparingTheMelodySoundTo(Guitar.FINGERBOARD_DIAGRAM, melody);
+            //1つの音に対し、候補となる座標をリスト形式でまとめる
+            List<int[]> candidate = comparingTheMelodySoundTo(Guitar.FINGERBOARD_DIAGRAM, melody);
+
+            //リストをリストとしてまとめる
             candidates.add(candidate);
         }
 
-        varianceList = sortCandidatesByVariance(candidates);
+        //組み合わせを考える
+        //組み合わせを作成
+        List<int[]> combination = new ArrayList<>();
+        List<List<int[]>> combinations = new ArrayList<>();
 
-        //距離の分散の小さい順にならべ、上位5~1件を提出
-        for(int i=0; i<5 && i<varianceList.size(); i++){
+        for(List<int[]> candidate : candidates) {
+            System.out.println("candidates : "+candidates.size());
+            for(int[] candidatePositon : candidate) {
+                System.out.println("candidate : "+candidate.size());
+                combination.add(candidatePositon);
+            }
+            combinations.add(combination);
+            System.out.println("combination : "+combination.size());
+        }
+
+        //各組み合わせの(距離の)分散を求め、小さい順にならべる
+        List<Double> varianceList = sortCandidatesByVariance(combinations);
+        System.out.println("戻されたvarianceList : "+varianceList.size());
+
+        //上位5~1件を戻り値とする
+        for(int i=0;  i<varianceList.size() && i<5 ; i++){
             System.out.println("varianceList.size() : "+varianceList.size());
             System.out.println("variance : "+String.valueOf(varianceList.get(i)));
         }
@@ -67,40 +86,48 @@ public class Process {
         return candidate;
     }
 
-    private List<Double> sortCandidatesByVariance(List<List<int[]>> candidates) {
+    private List<Double> sortCandidatesByVariance(List<List<int[]>> combinations) {
+        List<Double> varianceList = new ArrayList<>();
+
+        for(List<int[]> combination : combinations) {
+            varianceList.add(obtainTheVariance(combination));
+        }
+
+        Collections.sort(varianceList, Collections.reverseOrder());
+
         //小さい順に並べる =
         //各組み合わせで分散を算出。その組み合わせを保持
-        if(varianceList.size() == 0){
-            varianceList.add(obtainTheVariance(candidates));
-        }else{
-            //小さい順に並べる
-            for(int i=0; i<varianceList.size(); i++){
-                // TODO : 無限に増えている
-                System.out.println("size : "+varianceList.size());
-
-                double variance = varianceList.get(i);
-
-                if(variance <= obtainTheVariance(candidates)){
-                    varianceList.add(i,variance);
-                } else if (variance >= obtainTheVariance(candidates)) {
-                    varianceList.add(i+1,variance);
-                }
-            }
-        }
+//        for(List<int[]> combination : combinations){
+//            System.out.println("combinations : "+combinations.size());
+//            if(varianceList.size() == 0){
+//                varianceList.add(obtainTheVariance(combination));
+//            }else{
+//                //小さい順に並べる
+//                for(int i=0; i<varianceList.size(); i++){
+////                    System.out.println("varianceList : "+varianceList.size());
+//
+//                    if(varianceList.get(i) <= obtainTheVariance(combination)){
+//                        varianceList.add(i,obtainTheVariance(combination));
+//                        //TODO : 無限
+//                    } else {
+//                        varianceList.add(i+1,obtainTheVariance(combination));
+//                    }
+//                }
+//            }
+//        }
 
         return varianceList;
     }
 
-    public double obtainTheVariance(List<List<int[]>> candidates){
-        //ある組み合わせを取り出す(Listに格納)(拡張for)
+    public double obtainTheVariance(List<int[]> combination){
+        //座標の組み合わせを、全て距離に変換
         List<Double> melodiesForCalc = new ArrayList<>();
-        for(List<int[]> candidate : candidates){
-            for(int[] position : candidate){
-                melodiesForCalc.add(Math.sqrt(Math.pow(position[0],2)+Math.pow(position[1],2)));
-            }
+
+        for(int[] position : combination){
+            melodiesForCalc.add(Math.sqrt(Math.pow(position[0],2)+Math.pow(position[1],2)));
         }
 
-        //重心の座標を求め平均の座標とする(拡張for)
+        //重心の座標を求め平均の座標とする
         double average = 0;
         for (double melody : melodiesForCalc){
             average += melody/melodiesForCalc.size();
@@ -111,8 +138,6 @@ public class Process {
         for (double melody : melodiesForCalc){
             variance += Math.pow((melody - average),2);
         }
-
-        //√(1/n*Σ(xi-平均)**2)
 
         return variance;
     }
