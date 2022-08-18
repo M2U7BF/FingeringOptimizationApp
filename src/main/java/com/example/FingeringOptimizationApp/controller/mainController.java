d1c8a233d2725be5f00d6226ac36f1ccb6669d44
M2U7BF <yhvh4321@gmail.com>
2022-08-18 09:48:19 +0900

    [更新] メイン画面のメソッドを追加

--- src/main/java/com/example/FingeringOptimizationApp/controller/mainController.java
+++ src/main/java/com/example/FingeringOptimizationApp/controller/mainController.java
@@ -1,17 +1,19 @@
-package controller;
+package com.example.FingeringOptimizationApp.controller;
 
-import form.ConditionForm;
+import com.example.FingeringOptimizationApp.form.ConditionForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import service.Process;
+import com.example.FingeringOptimizationApp.service.Process;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("")
 public class mainController {
     @ModelAttribute
     public ConditionForm setUpForm() {
@@ -21,8 +23,8 @@ public class mainController {
     @Autowired
     Process process;
 
-    @GetMapping("")
-    public String index(ConditionForm f){
+    @GetMapping("home")
+    public String index(){
         return "index";
     }
 
