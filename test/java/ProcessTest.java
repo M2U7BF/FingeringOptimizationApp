6a18eaaf24a67945630c193f814d6b9b5de7c09e
M2U7BF <yhvh4321@gmail.com>
2022-09-04 13:01:51 +0900

    [更新] obtainTheVarianceメソッドのテストコードの追加

--- test/java/ProcessTest.java
+++ test/java/ProcessTest.java
@@ -2,6 +2,7 @@ import com.example.FingeringOptimizationApp.form.ConditionForm;
 import org.junit.jupiter.api.Test;
 import com.example.FingeringOptimizationApp.service.Process;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
@@ -49,19 +50,30 @@ public class ProcessTest {
     @Test
     void varianceTest(){
         Process process = new Process();
-        int[] testList = {0,0};
-        List<List<int[]>> test = List.of(List.of(testList));
-        List<int[]> test2 = List.of(testList);
+        List<int[]> test = List.of(
+            new int[]{0,1},
+            new int[]{0,1},
+            new int[]{1,0}
+        );
 
-        assertThat(0.0)
-                .as("結果が0に等しいか")
-                .isEqualTo(process.obtainTheVariance(test2));
+        //同値性 : 0に等しいか
+        checkObtainTheVariance(0.000, process.obtainTheVariance(test));
 
-        assertThat(process.obtainTheVariance(test2))
-                .as("0以上か")
-                .isGreaterThanOrEqualTo(0);
+        //0以上か
+    }
 
-    };
+    public void checkObtainTheVariance(double test, double variance){
+        //小数点3桁切り捨て
+        boolean isEqual =
+                test == (Math.floor(variance*1000))/1000;
+
+        if(isEqual){
+            // do nothing.
+        }else{
+            throw new RuntimeException("error!");
+        }
+    }
+
 //    @Test
 //    void 戻り値のList要素の型が適切(){
 //        Process process = new Process();
