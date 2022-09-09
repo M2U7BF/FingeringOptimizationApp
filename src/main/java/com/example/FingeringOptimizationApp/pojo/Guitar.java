1ddfa49bba36cd7d01999a0c2820350c676d733a
M2U7BF <yhvh4321@gmail.com>
2022-09-09 09:36:04 +0900

    [更新] オクターブのリストを追加。

--- src/main/java/com/example/FingeringOptimizationApp/pojo/Guitar.java
+++ src/main/java/com/example/FingeringOptimizationApp/pojo/Guitar.java
@@ -1,4 +1,4 @@
-package com.example.FingeringOptimizationApp.entity;
+package com.example.FingeringOptimizationApp.pojo;
 
 import lombok.Data;
 
@@ -13,4 +13,12 @@ public class Guitar {
             {"D","Em","E","F","G♭","G","Am","A","Bm","B","C","Dm"},
             {"A","Bm","B","C","Dm","D","Em","E","F","G♭","G","Am"},
             {"E","F","G♭","G","Am","A","Bm","B","C","Dm","D","Em"},};
+
+    public static final int[][] FINGERBOARD_OCTAVE =
+            {{2,2,2,2,2,2,2,2,2,2,2,2},
+            {1,1,1,1,1,2,2,2,2,2,2,2},
+            {1,1,1,1,1,1,1,1,1,2,2,2},
+            {0,0,1,1,1,1,1,1,1,1,1,1},
+            {0,0,0,0,0,0,0,1,1,1,1,1},
+            {0,0,0,0,0,0,0,0,0,0,0,0}};
 }
